fbe2c09b9102bf78bbbfceed24b83e7a0c5008e6
Aleksandrova Oksana <aleksandrovao767@gmail.com>
2023-08-19 12:54:39 +0300

    added style.css

---
+++ main.js
@@ -0,0 +1,27 @@
+const numberOne = 1;
+const numberTwo = 12.34;
+const firstName = 'Oksana';
+const surname = 'Aleksandrova';
+const boolenOne = true;
+const boolenTwo = false;
+
+// operation with numbers
+let summ = numberOne + numberTwo;
+let subtract = numberTwo - numberOne;
+let multi = numberOne * numberTwo;
+let divide = numberTwo / numberOne;
+let remains = numberTwo % numberOne;
+console.log(summ, subtract, multi, divide, remains);
+//typeof variables
+console.log(
+  typeof numberOne,
+  typeof numberTwo,
+  typeof firstName,
+  typeof surname,
+  typeof boolenOne,
+  typeof boolenTwo
+);
+//PART TWO
+let str = 'I learning javascript';
+const arrayWordds = str.split(',');
+console.log(arrayWordds);
