9769acaef247cbef511c2cb8aaaef372139e1666
Aleksandrova Oksana <aleksandrovao767@gmail.com>
2023-08-20 21:29:35 +0300

    added tag form

--- main.js
+++ main.js
@@ -6,26 +6,30 @@ let result;
 function selectChanged(value) {
   let numberOneValue = +numberOne.value;
   let numberTwoValue = +numberTwo.value;
+  if (typeof numberOneValue == 'string' || typeof numberTwoValue == 'string') {
+    alert('Введите число');
+  } else {
+    switch (value) {
+      case '1':
+        return (result = numberOneValue + numberTwoValue);
 
-  switch (value) {
-    case '1':
-      return (result = numberOneValue + numberTwoValue);
+        break;
+      case '4':
+        return (result = numberOneValue * numberTwoValue);
 
-      break;
-    case '4':
-      return (result = numberOneValue * numberTwoValue);
+        break;
+      case '2':
+        return (result = numberOneValue - numberTwoValue);
 
-      break;
-    case '2':
-      return (result = numberOneValue - numberTwoValue);
-
-      break;
-    case '3':
-      return (result = numberOneValue / numberTwoValue);
+        break;
+      case '3':
+        return (result = numberOneValue / numberTwoValue);
+    }
+    return result;
   }
-  return result;
 }
-function equals() {
+function equals(event) {
+  event.preventDefault();
   resultSpan.textContent = result;
 }
 equallyButton.addEventListener('click', equals);
